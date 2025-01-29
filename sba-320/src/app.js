bdc5a16b4a21a927747f45905fc172ead74c4759
AriMar159 <96703023+AriMar159@users.noreply.github.com>
2025-01-28 23:16:28 -0500

    set up components and app css

---
+++ sba-320/src/app.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Popular from './Components/Popular/Popular'
+import Offer from './Components/Offers/Offers'
+import Blog from './Components/Blog/blog'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+    return (
+        <>
+        <Navbar />
+        {/*<Home />
+        <Popular />
+        <Offer />
+        <About />
+        <Blog />
+        <Footer />}
+        </>
+    )
+}
+
+export default App
