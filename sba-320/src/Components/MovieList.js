ce3ace0acab47c41d946399214b63cfbf9dc8a80
AriMar159 <96703023+AriMar159@users.noreply.github.com>
2025-01-29 14:51:41 -0500

    finished adding movie display- adding scroll

--- sba-320/src/Components/MovieList.js
+++ sba-320/src/Components/MovieList.js
@@ -3,12 +3,14 @@ import React from 'react';
 const MovieList = (props) => {
     return (
         <>
-            {props.movies.map((movie, index)=> <div>
-                <img src={movie.Poster}></img>
-            </div>)}
+            {props.movies.map((movie, index)=> (
+                <div>
+                <img src={movie.Poster} alt='movie'></img>
+                </div>
+        ))}
         
         </>
-    )
-}
+    );
+};
 
 export default MovieList;
